6d662bba3d2b8ce4bddf16616b72eceee797f74f
Budhaditya Saha <budha.rocks@gmail.com>
2021-03-21 00:15:48 +0530

    Sec Script Trimmed

--- scripts/modules/05_pentools.sh
+++ scripts/modules/05_pentools.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sudo dnf update -y
-sudo dnf group install security-lab -y
-sudo dnf install hydra hydra-frontend gobuster hashcat scalpel foremost steghide whois aircrack-ng traceroute -y
+sudo dnf install hydra hydra-frontend gobuster hashcat scalpel foremost steghide whois aircrack-ng traceroute wireshark nmap -y
 
 mkdir -p /home/sam/SecTools
 mkdir -p /home/sam/SecTools/wordlists
@@ -16,9 +15,8 @@ mv SecLists /home/sam/SecTools/wordlists
 sudo dnf remove security-menus -y
 
 # John the Ripper
-# --TODO --
-
 # Compile process is long, better refer to - https://github.com/openwall/john/blob/bleeding-jumbo/doc/INSTALL-FEDORA
 
 # Clear Screen Output
+sleep 5
 clear
