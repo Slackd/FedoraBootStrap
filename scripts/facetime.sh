e73102f8c95a080519dbd27640f2811ae4a70c3c
Budhaditya Saha <budha.rocks@gmail.com>
2021-03-31 01:59:57 +0530

    fixes

--- scripts/facetime.sh
+++ scripts/facetime.sh
@@ -2,10 +2,10 @@
 
 # Very basic install script for the Facetime HD Driver for Linux
 # Software - Tested only on Fedora 33/34.
-# Hardware - MacBook Pro 15" 2015 Late (i5 Model)
-# Broadcom Inc. and subsidiaries 720p FaceTime HD - Camera
-# This is useful for Stock & Custom Kernels
-# This is totally manual pathway to install. You can read in the main Github for more info on DKMS auto setup.
+# Hardware - MacBook Pro 13" Late 2015 (i5 Model) --- Broadcom Inc. and subsidiaries 720p FaceTime HD - Camera
+# This is useful for building the module on both Stock & Custom Kernels
+# For Automating this --- You can read in the main Github for more info on DKMS auto setup.
+# Ref: https://github.com/patjak/bcwc_pcie/wiki/Installation#get-started-on-fedora
 
 # Basic Username Check
 read -p "Please enter your user name: " username
