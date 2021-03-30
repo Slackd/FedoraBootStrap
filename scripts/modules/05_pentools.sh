5ee11870427c9332c9495448e978fd6aed964602
Budhaditya Saha <budha.rocks@gmail.com>
2021-03-31 02:11:16 +0530

    Fix minor issues

--- scripts/modules/05_pentools.sh
+++ scripts/modules/05_pentools.sh
@@ -2,23 +2,25 @@
 
 sudo dnf update -y
 
+# Basic check for username
+read -p "Please enter your user name: " username
+
 # Sec Lab disabled by default as this has too many packages which are not needed.
 # Need to hand select the packages later
 
-#sudo dnf group install "Security Lab" -y
+# sudo dnf group install "Security Lab" -y
+# sudo dnf remove security-menus -y
 
 sudo dnf install hydra hydra-frontend gobuster hashcat scalpel foremost steghide whois aircrack-ng traceroute wireshark nmap -y
 
-mkdir -p /home/sam/SecTools
-mkdir -p /home/sam/SecTools/wordlists
+mkdir -p /home/$username/SecTools
+mkdir -p /home/$username/SecTools/wordlists
 
 wget -c https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
 git clone https://github.com/danielmiessler/SecLists.git
 
-mv rockyou.txt /home/sam/SecTools/wordlists
-mv SecLists /home/sam/SecTools/wordlists
-
-sudo dnf remove security-menus -y
+mv rockyou.txt /home/$username/SecTools/wordlists
+mv SecLists /home/$username/SecTools/wordlists
 
 # John the Ripper
 # Compile process is long, better refer to - https://github.com/openwall/john/blob/bleeding-jumbo/doc/INSTALL-FEDORA
