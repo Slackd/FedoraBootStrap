236955599a95b4cfa1b22b0bc202f6f759a8c049
Budhaditya Saha <budha.rocks@gmail.com>
2021-06-02 23:56:22 +0530

    remove flathub beta

--- scripts/modules/04_flatpak.sh
+++ scripts/modules/04_flatpak.sh
@@ -2,12 +2,9 @@
 
 # Flathub Remote Add the Flathub Repo.
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-# We are adding beta just for Gnome-Boxes. (Reconsider removing this, if the issues are fixed with gnome-nightly images.)
-flatpak remote-add --user flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 
 # Install Programs
-flatpak install flathub org.qbittorrent.qBittorrent com.spotify.Client com.github.micahflee.torbrowser-launcher org.telegram.desktop com.leinardi.gst org.gnome.Geary org.signal.Signal -y
-flatpak install --user flathub-beta com.obsproject.Studio
+flatpak install flathub com.obsproject.Studio org.qbittorrent.qBittorrent com.spotify.Client com.github.micahflee.torbrowser-launcher org.telegram.desktop com.leinardi.gst org.gnome.Geary org.signal.Signal -y
 
 # Clear Screen Output
 sleep 5
