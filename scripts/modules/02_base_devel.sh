a364820bac1d3f7f022949659f342d51221ddb76
Budhaditya Saha <budha.rocks@gmail.com>
2021-09-06 21:30:31 +0530

    Brave Removed

--- scripts/modules/02_base_devel.sh
+++ scripts/modules/02_base_devel.sh
@@ -5,8 +5,6 @@ sudo dnf install dnf-plugins-core -y
 
 # Adding 3P Repos for some packages
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-#sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-#sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
