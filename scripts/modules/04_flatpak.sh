ed7595377c2312eccacba91e2b5f8502496c85a8
Budhaditya Saha <budha.rocks@gmail.com>
2021-10-03 23:44:38 +0530

    added some new flatpak apps

--- scripts/modules/04_flatpak.sh
+++ scripts/modules/04_flatpak.sh
@@ -5,6 +5,10 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 # Install Programs
 flatpak install flathub com.obsproject.Studio org.qbittorrent.qBittorrent com.spotify.Client com.github.micahflee.torbrowser-launcher org.telegram.desktop com.leinardi.gst org.gnome.Geary org.signal.Signal -y
+flatpak install flathub de.haeckerfelix.Shortwave com.github.fabiocolacio.marker com.github.johnfactotum.Foliate com.github.tchx84.Flatseal -y
+flatpak install flathub com.gitlab.newsflash -y
+flatpak install flathub com.github.jeromerobert.pdfarranger -y
+flatpak install flathub org.gnome.Builder -y
 
 # Clear Screen Output
 sleep 5
